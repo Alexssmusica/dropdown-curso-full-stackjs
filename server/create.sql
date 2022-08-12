51f225d08ca195932cd6c3c480fc9e7225acd830
Alex Santos de Souza <41313437+Alexssmusica@users.noreply.github.com>
2022-08-12 13:12:55 -0300

    fix: nome schemas padrao public

--- server/create.sql
+++ server/create.sql
@@ -1,32 +1,32 @@
-drop table if exists dropdown.card;
-drop table if exists dropdown.column;
-drop table if exists dropdown.board;
+drop table if exists public.card;
+drop table if exists public.column;
+drop table if exists public.board;
 
-create table dropdown.board (
+create table public.board (
 	id_board serial primary key,
 	name text
 );
 
-create table dropdown.column (
+create table public.column (
 	id_column serial primary key,
-	id_board integer references dropdown.board (id_board),
+	id_board integer references public.board (id_board),
 	name text,
 	has_estimative boolean,
 	index integer
 );
 
-create table dropdown.card (
+create table public.card (
 	id_card serial primary key,
-	id_column integer references dropdown.column (id_column),
+	id_column integer references public.column (id_column),
 	title text,
 	estimative integer,
 	index integer
 );
 
-insert into dropdown.board (name) values ('Projeto 1');
-insert into dropdown.column (id_board, name, has_estimative) values (1, 'Coluna A', true);
-insert into dropdown.column (id_board, name, has_estimative) values (1, 'Coluna B', true);
-insert into dropdown.column (id_board, name, has_estimative) values (1, 'Coluna C', true);
-insert into dropdown.card (id_column, title, estimative) values (1, 'Atividade 1', 3);
-insert into dropdown.card (id_column, title, estimative) values (1, 'Atividade 2', 2);
-insert into dropdown.card (id_column, title, estimative) values (1, 'Atividade 3', 1);
+insert into public.board (name) values ('Projeto 1');
+insert into public.column (id_board, name, has_estimative) values (1, 'Coluna A', true);
+insert into public.column (id_board, name, has_estimative) values (1, 'Coluna B', true);
+insert into public.column (id_board, name, has_estimative) values (1, 'Coluna C', true);
+insert into public.card (id_column, title, estimative) values (1, 'Atividade 1', 3);
+insert into public.card (id_column, title, estimative) values (1, 'Atividade 2', 2);
+insert into public.card (id_column, title, estimative) values (1, 'Atividade 3', 1);
--- server/src/infra/repository/CardRepositoryDatabase.ts
+++ server/src/infra/repository/CardRepositoryDatabase.ts
@@ -8,7 +8,7 @@ export default class CardRepositoryDatabase implements CardRepository {
 	}
 
 	async findAllByIdColumn(idColumn: number): Promise<Card[]> {
-		const cardsData = await this.connection.query("select id_column, id_card, title, estimative from dropdown.card where id_column = $1", [idColumn]);
+		const cardsData = await this.connection.query("select id_column, id_card, title, estimative from public.card where id_column = $1", [idColumn]);
 		const cards: Card[] = [];
 		for (const cardData of cardsData) {
 			cards.push(new Card(cardData.id_column, cardData.id_card, cardData.title, cardData.estimative));
@@ -17,16 +17,16 @@ export default class CardRepositoryDatabase implements CardRepository {
 	}
 
 	async save(card: Card): Promise<number> {
-		const [cardData] = await this.connection.query("insert into dropdown.card (id_column, title, estimative) values ($1, $2, $3) returning *", [card.idColumn, card.title, card.estimative]);
+		const [cardData] = await this.connection.query("insert into public.card (id_column, title, estimative) values ($1, $2, $3) returning *", [card.idColumn, card.title, card.estimative]);
 		return cardData.id_card;
 	}
 
 	async update(card: Card): Promise<void> {
-		await this.connection.query("update dropdown.card set title = $1, estimative = $2 where id_card = $3", [card.title, card.estimative, card.idCard]);
+		await this.connection.query("update public.card set title = $1, estimative = $2 where id_card = $3", [card.title, card.estimative, card.idCard]);
 	}
 	
 	async delete(idCard: number): Promise<void> {
-		await this.connection.query("delete from dropdown.card where id_card = $1", [idCard]);
+		await this.connection.query("delete from public.card where id_card = $1", [idCard]);
 	}
 
 }
